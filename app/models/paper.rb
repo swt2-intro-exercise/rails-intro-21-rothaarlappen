f82d04a2379e11a8ba9352e6d425e4f3a932f92e
Rothaarlappen <roessler.paul@web.de>
2021-11-23 23:44:51 +0100

    adds has and belongs to many info to paper & author

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,5 @@ class Paper < ApplicationRecord
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: true
+    has_and_belongs_to_many :authors
 end
